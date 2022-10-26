a2584d96030811ccacc0d1a9f3a97f52692d4c0f
Joshua Smith <joshua.barton.smith12@gmail.com>
2022-10-26 14:04:55 -0400

    add docstrings for auth.py

--- hive/auth.py
+++ hive/auth.py
@@ -1,30 +1,45 @@
 from .utils import hive_signed_request
 from .constants import HIVE_API_GUEST_GENERATE, HIVE_API_GUEST_LOGIN
 
-# Superclass for Hive authentication.
 class HiveUser:
+    """Superclass for Hive authentication which should not be used directly."""
     HIVE_UID = None
     PEPPERMINT_TOKEN = None
 
     def __init__(self, uid = None):
+        """
+        Initialize a Hive user without authentication information.
+        Parameters
+        ----------
+        uid : int or string
+            If provided, Hive will attempt to use the provided UID for authentication.
+        """
         self.HIVE_UID = uid
         self.PEPPERMINT_TOKEN = None
 
     def authenticate(self):
+        """Authenticate a Hive user using information provided. This should be implemented by concrete Hive authentication types."""
         raise NotImplementedError
 
 # Full Hive user - currently unsupported. Intended for use with login credentials.
 class HiveFullUser(HiveUser):
+    """Represents a full Hive user with a verified account. Currently support for full Hive users is limited."""
     def authenticate(self, uid = None):
         raise HiveAuthException("Full Hive users are not yet supported. Use HiveGuestUser instead.")
 
 # Represents a Guest user to the Hive authentication system. Guest users are nice and simple to use.
 # The HiveGuestUser class will handle authentication steps, and can optionally use a pre-existing guest UID to avoid the overhead of setting up a new Guest every time the system is restarted.
 class HiveGuestUser(HiveUser):
+    """Represents a Hive guest user without concrete authentication details."""
+
     def __init__(self, uid = None):
         super().__init__(uid)
 
     def authenticate(self):
+        """
+        Authenticates a Hive guest user using the UID provided. If no UID was provided, a new UID will be automatically generated for the user.
+        It's recommended to generate a UID once, then provide the same UID for authentication as a guest in the future.
+        """
         # no need to re-auth if peppermint token already filled
         if self.PEPPERMINT_TOKEN != None: return
 
